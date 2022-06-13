5245034594c40bd901fac4a349db17f3e0adf183
Pranav <pranavtri00@gmail.com>
2022-06-14 00:43:26 +0530

    Portfolio Section

--- src/assets/data/portfolio.js
+++ src/assets/data/portfolio.js
@@ -2,24 +2,45 @@ const portfolio = [
   {
     id: 1,
     title: "Aveksha",
-    image: "",
-    link: "",
-    date: "",
+    image: "/images/Aveksha.png",
+    link: "https://github.com/PranavTrip/Rowdyhacks",
+    date: "April,2021"
   },
   {
     id: 2,
     title: "DonateForCause",
-    image: "",
-    link: "",
-    date: "",
+    image: "/images/DonateForCause.png",
+    link: "https://github.com/PranavTrip/DonateforCause",
+    date: "March,2021"
   },
   {
     id: 3,
-    title: "Fitness Freaks",
-    image: "",
-    link: "",
-    date: "",
+    title: "EasyPay",
+    image: "/images/EasyPay.png",
+    link: "https://github.com/PranavTrip/EcommerceUI",
+    date: "October,2021"
   },
+  {
+    id: 4,
+    title: "WeatherBook",
+    image: "/images/WeatherBook.png",
+    link: "https://github.com/PranavTrip/WeatherReact",
+    date: "October,2021"
+  },
+  {
+    id: 5,
+    title: "CovidDashBoard",
+    image: "/images/CovidDashBoard.png",
+    link: "https://github.com/PranavTrip/Covid-19-dashboard-India-",
+    date: "August,2021"
+  },
+  {
+    id: 6,
+    title: "PassWordManager",
+    image: "/images/PasswordManager.png",
+    link: "https://github.com/PranavTrip/PasswordManager",
+    date: "April,2022"
+  }
 ];
 
 export default portfolio;
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import portfolio from '../assets/data/portfolio';
+import "../assets/css/Portfolio.css"
 
 function Portfolio() {
   return (
@@ -10,7 +12,20 @@ function Portfolio() {
             <p className="portfolio-s-text">For a complete list of my PROJECTS please visit my <a href="http://github.com/PranavTrip" target="_blank"><i className="fa-brands fa-github"></i></a></p>
         </div>
         <div className="portfolio-image-section">
-
+            <div className="portfolio-image-container">
+              {portfolio.map((element)=>{
+                  return(
+                    <div className='portfolio-data' key={element.id}>
+                      <img src={element.image} alt="portfolio-image" className="portfolio-image img-fluid"/>
+                      <p className="portfolio-description">
+                        {element.title}
+                      </p>
+                      <small><a href={element.link}>Link</a></small>
+                      <small>{element.date}</small>
+                    </div>
+                  )
+              })}
+            </div>
         </div>
       </div>
     </>
